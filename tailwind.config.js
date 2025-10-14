e73231f9b5da38c5bb6f29f9164ed562e41d9660
Nama Kamu <emailkamu@example.com>
2025-10-14 13:06:39 +0700

    fix eroor

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: '#bfdbfe', // blue-100
+          DEFAULT: '#3b82f6', // blue-500
+          dark: '#1e40af', // blue-800
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', 'Poppins', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
